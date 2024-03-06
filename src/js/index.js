9f244c1b5219d4123623100ccf679243c69c8d05
johnson <johnson@basics.fun>
2024-03-06 09:12:26 +0530

    added toogle

--- src/js/index.js
+++ src/js/index.js
@@ -5,3 +5,29 @@ function clearDefaultValue(input) {
     }
 
 }
+
+function toggleAdditionalInfo(clickedElement) {
+    // Find the closest parent list item (<li>)
+    var listItem = clickedElement.closest('li');
+
+    // Toggle the 'selected' class on the list item
+    listItem.classList.toggle('selected');
+
+    // Toggle the 'selected' class on the additional info container
+    var additionalInfo = listItem.querySelector('.additonal-info');
+    additionalInfo.classList.toggle('selected');
+
+    // Toggle the image source based on the 'selected' class
+    var toggleIcon = listItem.querySelector('.toogle-icon');
+    
+    toggleIcon.src = (listItem.classList.contains('selected')) ? 'src/img/icons8-minus-32.png' : 'src/img/icons8-plus-24.svg';
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+var yourElement = document.getElementById("education-container");
+
+// Scroll to the element with different behaviors
+yourElement.scrollIntoView({ behavior: "auto" });
+// Uncomment the line below to try "smooth" behavior
+// yourElement.scrollIntoView({ behavior: "smooth" });
+});
